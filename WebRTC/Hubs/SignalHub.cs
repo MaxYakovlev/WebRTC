fdf76959dafbfb4d2f1b4265ef04bab6cb895819
MaxYakovlev <y.m.s.21@mail.ru>
2021-01-08 03:04:52 +0300

    make simple video-audio call

---
+++ WebRTC/Hubs/SignalHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRTC.Hubs
+{
+    public class SignalHub : Hub
+    {
+        public async Task Send(object data)
+        {
+            await Clients.Others.SendAsync("Send", data);
+        }
+    }
+}
